1781f6fad3e000f7eba16a498f6040d2c56ef065
Thiago Rocha <thiago_srocha01@hotmail.com>
2025-05-11 16:25:28 -0300

    feat(0h38): altera globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant={"destructive"} size={"lg"}>
-        Hello World
-      </Button>
+    <div className='flex flex-col gap-4 p-20'>
+      <div>
+        <Button>I am a button</Button>
+      </div>
+      <div>
+        <Input placeholder='i am an input' />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder='i am a textarea' />
+      </div>
     </div>
   );
 }
